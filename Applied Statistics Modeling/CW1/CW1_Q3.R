f983e849ebf42e6d9a3c3985b7731e9a0ce6fa6f
BL-Starlord <1021618717@qq.com>
2024-03-27 03:13:28 +0100

    update cw

--- Applied Statistics Modeling/CW1/CW1_Q3.R
+++ Applied Statistics Modeling/CW1/CW1_Q3.R
@@ -2,11 +2,13 @@ data <- read.csv("/Users/ryan/Documents/GitHub/Statistics/Applied Statistics Mod
 # Source the script containing the function
 source("/Users/ryan/Documents/GitHub/Statistics/Applied Statistics Modeling/CW1/CW1_Q3_functions.R")
 
+library(ggplot2)
+
 
 default = data[, 2]
 default = factor(default)
 
-#default_num<- as.numeric(default) - 1
+default_num<- as.numeric(default) - 1
 
 student <- data[, 3]
 student <- factor(data$student, levels = c("No", "Yes"))
@@ -16,21 +18,44 @@ income <- data[, 5]
 
 
 
+
+
 #Qa
 correlation1 <- cor(default_num, balance)
 correlation2 <- cor(default_num, income)
 correlation1
 correlation2
 
+ggplot(data, aes(x = default, y = balance)) +
+  geom_boxplot() +
+  labs(title = "Box plot of Balance by Default Status",
+       x = "Default Status",
+       y = "Balance") +
+  theme_minimal()
+
+ggplot(data, aes(x = default, y = income)) +
+  geom_boxplot() +
+  labs(title = "Box plot of income by Default Status",
+       x = "Default Status",
+       y = "income") +
+  theme_minimal()
+
+
 #Qc
 out1 = glm(default ~ student + balance + income, family=binomial(link="logit"))
 #out1_num = glm(default_num ~ student + balance + income, family=binomial(link="logit"))
+
+# Assuming 'out1' is your glm model
+confint_out1 <- confint(out1, "balance", level = 0.95)
+confint_out2 <- confint(out1, "income", level = 0.95)
+print(confint_out1)
+print(confint_out2)
 
 summary(out1)
 
 vcov(out1)
 
-summary(out1_num)
+#summary(out1_num)
 
 #Qd
 
@@ -40,17 +65,16 @@ mbp1
 mp1 <- max_probability(data,out1)
 mp1
 
-mbp1_num <- max_balance_probability(data,out1_num)
-mbp1_num
-
-mp1_num <- max_probability(data,out1_num)
-mp1_num
+# mbp1_num <- max_balance_probability(data,out1_num)
+# mbp1_num
+# 
+# mp1_num <- max_probability(data,out1_num)
+# mp1_num
 
 #Qe
 out1 = glm(default ~ student + balance + income, family=binomial(link="logit"))
 out2 <- glm(default ~ student + balance + income, family=binomial(link="probit"))
 out3 <- glm(default ~ student + balance + income, family=binomial(link="cloglog"))
-out4 <- glm(default ~ student + balance + income, family=binomial(link="cauchit"))
 
 mbp2 <- max_balance_probability(data,out2)
 mbp2
@@ -64,20 +88,14 @@ mbp3
 mp3 <- max_probability(data,out3)
 mp3
 
-mbp4 <- max_balance_probability(data,out4)
-mbp4
-
-mp4 <- max_probability(data,out4)
-mp4
-
-
 # Create a matrix with 4 rows and 2 columns
-results_table <- matrix(c(mbp1, mbp2, mbp3, mbp4, mp1, mp2, mp3, mp4), 
-                        nrow = 4, ncol = 2, byrow = FALSE)
+results_table <- matrix(c(mbp1[2], mbp2[2], mbp3[2], mbp1[1],mbp2[1],mbp3[1],mp1[2], mp2[2], mp3[2],mp1[1], mp2[1], mp3[1]), 
+                        nrow = 3, ncol = 4, byrow = FALSE)
 
 # Name the rows and columns
-rownames(results_table) <- c("logit", "probit", "cloglog", "cauchit")
-colnames(results_table) <- c("mbp", "mp")
+rownames(results_table) <- c("logit", "probit", "cloglog")
+colnames(results_table) <- c("mbp", "mbp_index","mp","mp_index")
 
 # Print the table
 print(results_table)
+
--- Applied Statistics Modeling/CW1/CW1_Q3_functions.R
+++ Applied Statistics Modeling/CW1/CW1_Q3_functions.R
@@ -3,6 +3,7 @@ max_balance_probability <- function(datasource, model) {
   max_balance <- max(datasource$balance)
   
   # Get the row(s) with the maximum balance
+  index_with_max_balance <- which(datasource$balance == max_balance)
   row_with_max_balance <- datasource[datasource$balance == max_balance, ]
   
   # Remove the 'default' column
@@ -11,28 +12,23 @@ max_balance_probability <- function(datasource, model) {
   # Predict probabilities for the new data
   new_predicted_probabilities <- predict(model, newdata = row_with_max_balance, type = "response")
   
-  # Return the predicted probabilities for the new data
-  return(new_predicted_probabilities)
+  # Return the results
+  results <- data.frame(index = index_with_max_balance, predicted_probabilities = new_predicted_probabilities)
+  return(results)
 }
 
 max_probability <- function(datasource, model) {
   # Predict probabilities for the entire dataset
-  predicted_probabilities <- predict(model, type = "response")
+  predicted_probabilities <- predict(model,newdata = datasource, type = "response")
   
   # Find the maximum predicted default probability
   max_predicted_default_probability <- max(predicted_probabilities)
   
+  # Get the row(s) with the maximum balance
+  index_of_max_probability <- which.max(predicted_probabilities)
+  
   # Return the maximum predicted default probability
-  return(max_predicted_default_probability)
+  results <- data.frame(index = index_of_max_probability, max_probabilities = max_predicted_default_probability)
+  return(results)
 }
 
-mbp <- function(datasource, model) {
-  # Predict probabilities for the entire dataset
-  predicted_probabilities <- predict(model, type = "response")
-  
-  # Find the maximum predicted default probability
-  max_predicted_default_probability <- max(predicted_probabilities)
-  
-  # Return the maximum predicted default probability
-  return(max_predicted_default_probability)
-}
